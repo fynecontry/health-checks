8bc1fdfdf83e5579c70ed1f77a4d91f7e67c7cfc
Gabriel Fynecontry <fynecontry@gmail.com>
2020-05-30 15:08:35 +0100

    Check for CPU load and rename all_checks.py to health_checks.py

--- health_checks.py
+++ health_checks.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import sys
+import psutil
 
 def check_reboot():
     """Returns True if a reboot is required"""
@@ -22,10 +23,15 @@ def check_root_full():
     """Returns  True if root partition is full, False otherwise"""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_cpu_constrained():
+    """Returns True if the CPU is having too much usage, False otherwise"""
+    return psutil.cpu_percent(1) > 75
+
 def main():
     checks=[
         (check_reboot,"Pending Reboot"),
         (check_root_full, "Root partition full"),
+        (check_cpu_constrained, "CPU load is too high"),
     ]
 
     everything_ok = True
